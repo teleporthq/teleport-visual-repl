d30556cd4d5be6312796f1f0a4ccf64bfb988a56
Ytrrium <t.cernusca@gmail.com>
2020-01-29 17:17:41 +0200

    Added some more functionality to UIDLParser

--- utils/UIDLParser.ts
+++ utils/UIDLParser.ts
@@ -1,42 +1,50 @@
 import { UIDLNode } from "../interfaces/UIDL";
 const myimport = require("./componentfile");
 const htmlMap = require("../utils/html-mapping.json");
-console.log(htmlMap);
 
-let depth = 0;
+
 const getComponent = (tree: UIDLNode, depth: number) => {
+  
   const contents = tree.content;
   let list: any[] = [];
   if (!contents) return [];
-  console.log(Object.keys(contents));
+  
+  let myResult = { depth : depth }
+  list.push(myResult);
+  
   Object.keys(contents).forEach(key => {
-    const elementType = contents[key];
-    if (typeof elementType === "string") {
-      if (key === "elementType") {
-        let myResult = {
-          elementType: htmlMap.elements[elementType]
-            ? htmlMap.elements[elementType].elementType
-            : elementType,
-          depth: depth
-        };
-        list.push(myResult);
+    const keyContents = contents[key];
+    if (typeof keyContents === "string") {
+      //if the key is elementType then use 
+      //the html mappings to try and translate it
+      if(key === "elementType"){
+        const element = htmlMap.elements[keyContents]
+        ? htmlMap.elements[keyContents].elementType
+        : keyContents
+        list[list.length - 1]
+        ? (list[list.length - 1][key] = element)
+        : null;
+
       } else {
         list[list.length - 1]
-          ? (list[list.length - 1][key] = elementType)
+          ? (list[list.length - 1][key] = keyContents)
           : null;
       }
     } else {
       if (key === "children") {
-        elementType.forEach(
+        keyContents.forEach(
           (child: UIDLNode) =>
             (list = [...list, ...getComponent(child, depth + 1)])
         );
+      } else if(key === "node"){
+        list = [...list, ...getComponent(keyContents, depth + 1)]
       } else {
-        list[list.length - 1][key] = elementType;
+        //if there are attributes after the children attribute this will not work
+        list[list.length - 1][key] = keyContents;
       }
     }
   });
   return list;
 };
-console.log("Result: ", getComponent(myimport.node, depth));
+console.log("Result: ", getComponent(myimport.node, 0));
 export default {};
--- utils/componentfile.js
+++ utils/componentfile.js
@@ -1,132 +1,275 @@
 const myComponent = {
-  name: "Navbar",
-  node: {
-    type: "element",
-    content: {
-      elementType: "nav",
-      style: {
-        "border-bottom": {
-          type: "static",
-          content: "1px solid rgba(0,0,0,0.25)"
-        }
-      },
-      children: [
+  "name": "TabSelector",
+  "stateDefinitions": {
+    "activeTab": {
+      "type": "number",
+      "defaultValue": 0
+    }
+  },
+  "node": {
+    "type": "element",
+    "content": {
+      "elementType": "container",
+      "children": [
         {
-          type: "element",
-          content: {
-            elementType: "ul",
-            name: "list-container",
-            style: {
-              "list-style": "none",
-              margin: "0px",
-              padding: "0px",
-              display: "flex",
-              "font-size": "18px"
+          "type": "element",
+          "content": {
+            "elementType": "container",
+            "name": "tablist",
+            "style": {
+              "margin": "0 0 -1px",
+              "overflow": "visible"
             },
-            children: [
+            "attrs": {
+              "role": "tablist"
+            },
+            "children": [
               {
-                type: "element",
-                content: {
-                  elementType: "li",
-                  name: "home-link",
-                  style: {
-                    padding: "10px 20px",
-                    transition: "all .15s ease-in-out",
-                    cursor: "pointer",
-                    "&:hover": {
-                      type: "nested-style",
-                      content: {
-                        color: {
-                          type: "static",
-                          content: "red"
-                        },
-                        "border-bottom": "3px solid red",
-                        "padding-bottom": "7px"
-                      }
-                    }
+                "type": "element",
+                "content": {
+                  "elementType": "button",
+                  "children": [
+                    "Tab One"
+                  ],
+                  "attrs": {
+                    "role": "tab",
+                    "data-active-tab": "$state.activeTab"
                   },
-                  children: [
-                    {
-                      type: "element",
-                      content: {
-                        elementType: "navlink",
-                        attrs: {
-                          transitionTo: "home"
-                        },
-                        children: ["Home"]
+                  "events": {
+                    "click": [
+                      {
+                        "type": "stateChange",
+                        "modifies": "activeTab",
+                        "newState": 0
                       }
+                    ]
+                  },
+                  "style": {
+                    "fontSize": "18px",
+                    "marginRight": "6px",
+                    "position": "relative",
+                    "overflow": "visible",
+                    "cursor": "pointer",
+                    "borderTop": "1px solid #ccc",
+                    "borderLeft": "1px solid #ccc",
+                    "borderRight": "1px solid #ccc",
+                    "boxShadow": "0 0 0.2em hsl(219, 1%, 72%)",
+                    "padding": "5px",
+                    "outline": "0",
+                    "background-color": "#eee",
+                    "&:focus": {
+                      "border-top": "3px solid orange",
+                      "padding-top": "3px"
+                    },
+                    "&[data-active-tab='0']": {
+                      "border-top": "3px solid green",
+                      "padding-top": "3px",
+                      "background-color": "white"
+                    },
+                    "&[data-active-tab='0']::after": {
+                      "bottom": "-1px",
+                      "position": "absolute",
+                      "height": "5px",
+                      "right": "0",
+                      "left": "0",
+                      "background-color": "white",
+                      "content": "' '",
+                      "z-index": "3"
+                    },
+                    "&:hover": {
+                      "border-top": "3px solid orange",
+                      "padding-top": "3px"
                     }
-                  ]
+                  }
                 }
               },
               {
-                type: "element",
-                content: {
-                  name: "team-link",
-                  elementType: "li",
-                  style: {
-                    padding: "10px 20px",
-                    transition: "all .15s ease-in-out",
-                    cursor: "pointer",
-                    "&:hover": {
-                      type: "nested-style",
-                      content: {
-                        color: {
-                          type: "static",
-                          content: "red"
-                        },
-                        "border-bottom": "3px solid red",
-                        "padding-bottom": "7px"
-                      }
-                    }
+                "type": "element",
+                "content": {
+                  "elementType": "button",
+                  "children": [
+                    "Tab Two"
+                  ],
+                  "attrs": {
+                    "role": "tab",
+                    "data-active-tab": "$state.activeTab"
                   },
-                  children: [
-                    {
-                      type: "element",
-                      content: {
-                        elementType: "navlink",
-                        attrs: {
-                          transitionTo: "team"
-                        },
-                        children: ["Team"]
+                  "events": {
+                    "click": [
+                      {
+                        "type": "stateChange",
+                        "modifies": "activeTab",
+                        "newState": 1
                       }
+                    ]
+                  },
+                  "style": {
+                    "fontSize": "18px",
+                    "marginRight": "6px",
+                    "position": "relative",
+                    "overflow": "visible",
+                    "cursor": "pointer",
+                    "borderTop": "1px solid #ccc",
+                    "borderLeft": "1px solid #ccc",
+                    "borderRight": "1px solid #ccc",
+                    "boxShadow": "0 0 0.2em hsl(219, 1%, 72%)",
+                    "outline": "0",
+                    "padding": "5px",
+                    "background-color": "#eee",
+                    "&:focus": {
+                      "border-top": "3px solid orange",
+                      "padding-top": "3px"
+                    },
+                    "&[data-active-tab='1']": {
+                      "border-top": "3px solid green",
+                      "padding-top": "3px",
+                      "background-color": "white"
+                    },
+                    "&[data-active-tab='1']::after": {
+                      "bottom": "-1px",
+                      "position": "absolute",
+                      "height": "5px",
+                      "right": "0",
+                      "left": "0",
+                      "background-color": "white",
+                      "content": "' '",
+                      "z-index": "3"
+                    },
+                    "&:hover": {
+                      "border-top": "3px solid orange",
+                      "padding-top": "3px"
                     }
-                  ]
+                  }
                 }
               },
               {
-                type: "element",
-                content: {
-                  elementType: "li",
-                  name: "contact-link",
-                  style: {
-                    padding: "10px 20px",
-                    transition: "all .15s ease-in-out",
-                    cursor: "pointer",
-                    "&:hover": {
-                      type: "nested-style",
-                      content: {
-                        color: {
-                          type: "static",
-                          content: "red"
-                        },
-                        "border-bottom": "3px solid red",
-                        "padding-bottom": "7px"
+                "type": "element",
+                "content": {
+                  "elementType": "button",
+                  "children": [
+                    "Tab Three"
+                  ],
+                  "attrs": {
+                    "role": "tab",
+                    "data-active-tab": "$state.activeTab"
+                  },
+                  "events": {
+                    "click": [
+                      {
+                        "type": "stateChange",
+                        "modifies": "activeTab",
+                        "newState": 2
                       }
+                    ]
+                  },
+                  "style": {
+                    "fontSize": "18px",
+                    "marginRight": "6px",
+                    "position": "relative",
+                    "overflow": "visible",
+                    "cursor": "pointer",
+                    "borderTop": "1px solid #ccc",
+                    "borderLeft": "1px solid #ccc",
+                    "borderRight": "1px solid #ccc",
+                    "boxShadow": "0 0 0.2em hsl(219, 1%, 72%)",
+                    "padding": "5px",
+                    "outline": "0",
+                    "background-color": "#eee",
+                    "&:focus": {
+                      "border-top": "3px solid orange",
+                      "padding-top": "3px"
+                    },
+                    "&[data-active-tab='2']": {
+                      "border-top": "3px solid green",
+                      "padding-top": "3px",
+                      "background-color": "white"
+                    },
+                    "&[data-active-tab='2']::after": {
+                      "bottom": "-1px",
+                      "position": "absolute",
+                      "height": "5px",
+                      "right": "0",
+                      "left": "0",
+                      "background-color": "white",
+                      "content": "' '",
+                      "z-index": "3"
+                    },
+                    "&:hover": {
+                      "border-top": "3px solid orange",
+                      "padding-top": "3px"
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        },
+        {
+          "type": "element",
+          "content": {
+            "elementType": "container",
+            "name": "tabPanel",
+            "style": {
+              "height": "300px",
+              "borderBottom": "1px solid #ccc",
+              "borderLeft": "1px solid #ccc",
+              "borderRight": "1px solid #ccc",
+              "boxShadow": "0 0 0.2em hsl(219, 1%, 72%)",
+              "backgroundColor": "white",
+              "position": "relative",
+              "z-index": "2",
+              "padding": "5px"
+            },
+            "children": [
+              {
+                "type": "conditional",
+                "content": {
+                  "node": {
+                    "type": "static",
+                    "content": "This text is in tab number 1"
+                  },
+                  "reference": {
+                    "type": "dynamic",
+                    "content": {
+                      "referenceType": "state",
+                      "id": "activeTab"
                     }
                   },
-                  children: [
-                    {
-                      type: "element",
-                      content: {
-                        elementType: "navlink",
-                        attrs: {
-                          transitionTo: "contact-us"
-                        },
-                        children: ["Contact Us"]
-                      }
+                  "value": 0
+                }
+              },
+              {
+                "type": "conditional",
+                "content": {
+                  "node": {
+                    "type": "static",
+                    "content": "This text is in tab number 2"
+                  },
+                  "reference": {
+                    "type": "dynamic",
+                    "content": {
+                      "referenceType": "state",
+                      "id": "activeTab"
                     }
-                  ]
+                  },
+                  "value": 1
+                }
+              },
+              {
+                "type": "conditional",
+                "content": {
+                  "node": {
+                    "type": "static",
+                    "content": "This text is in tab number 3"
+                  },
+                  "reference": {
+                    "type": "dynamic",
+                    "content": {
+                      "referenceType": "state",
+                      "id": "activeTab"
+                    }
+                  },
+                  "value": 2
                 }
               }
             ]
@@ -135,6 +278,6 @@ const myComponent = {
       ]
     }
   }
-};
+}
 
-module.exports = myComponent;
+module.exports = myComponent
