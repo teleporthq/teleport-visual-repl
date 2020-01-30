d3d507bc19b7b508c9532e04c2754bb1e135a539
vladgrecu <vladgrecu.smp@gmail.com>
2020-01-30 10:31:00 +0200

    changed UILDParser function

--- interfaces/UIDL.ts
+++ interfaces/UIDL.ts
@@ -2,3 +2,15 @@ export interface UIDLNode {
   type: string;
   content: any;
 }
+
+export interface UIDLElementNode {
+  type: "element";
+  content: {
+    elementType: string;
+    name?: string;
+    style?: any;
+    attrs?: any;
+    events?: any;
+    children?: UIDLNode[];
+  };
+}
--- utils/UIDLParser.ts
+++ utils/UIDLParser.ts
@@ -2,51 +2,60 @@ import { UIDLNode } from "../interfaces/UIDL";
 const myimport = require("./componentfile");
 const htmlMap = require("../utils/html-mapping.json");
 
-const getComponent = (tree: UIDLNode, depth: number = 0) => {
-  const contents = tree.content;
-  let list: any[] = [];
+// const getComponent = (tree: UIDLNode, depth: number = 0) => {
+//   const contents = tree.content;
+//   let list: any[] = [];
+//   if (!contents) {
+//     return [];
+//   }
+//   Object.keys(contents).forEach(key => {
+//     const elementType = contents[key];
+//     if (typeof elementType === "string") {
+//       if (key === "elementType") {
+//         let myResult = {
+//           elementType: htmlMap.elements[elementType]
+//             ? htmlMap.elements[elementType].elementType
+//             : elementType,
+//           depth: depth
+//         };
+//         list.push(myResult);
+//       } else {
+//         list.length ? (list[list.length - 1][key] = elementType) : null;
+//       }
+//     } else {
+//       if (key === "children") {
+//         elementType.forEach(
+//           (child: UIDLNode) =>
+//             (list = [...list, ...getComponent(child, depth + 1)])
+//         );
+//       } else {
+//         list[list.length - 1][key] = elementType;
+//       }
+//     }
+//   });
+//   return list;
+// };
 
-  if (!contents) {
-    return [];
-  }
-
-  let myResult = { depth: depth };
-  list.push(myResult);
-
-  Object.keys(contents).forEach(key => {
-    const keyContents = contents[key];
-    if (typeof keyContents === "string") {
-      //if the key is elementType then use
-      //the html mappings to try and translate it
-      if (key === "elementType") {
-        const element = htmlMap.elements[keyContents]
-          ? htmlMap.elements[keyContents].elementType
-          : keyContents;
-        list[list.length - 1] ? (list[list.length - 1][key] = element) : null;
-
-        return;
-      }
-      return;
-      // exit fast. Return and skip the 'else' statement
-    } else {
-      if (key === "children") {
-        keyContents.forEach(
-          (child: UIDLNode) =>
-            (list = [...list, ...getComponent(child, depth + 1)])
-        );
-        return;
-      }
-      if (key === "node") {
-        list = [...list, ...getComponent(keyContents, depth + 1)];
-        return;
-      }
+const flatten = (obj, depthLevel = 0) => {
+  const array = Array.isArray(obj) ? obj : [obj];
+  return array.reduce((acc, value) => {
+    let myResult = htmlMap.elements[value.elementType]
+      ? htmlMap.elements[value.elementType].elementType
+      : value.elementType;
+    if (value.elementType) {
+      value.elementType = myResult;
     }
-    list.length ? (list[list.length - 1][key] = keyContents) : null;
-
-    // try to call list[list.length - 1][key] = elementType; here, insead
-    // of the else branches above
-  });
-  return list;
+    acc.push({ elementInfo: value, depthLevel });
+    if (value.children) {
+      value.children.map(
+        child => (acc = [...acc, ...flatten(child.content, depthLevel + 1)])
+      );
+      delete value.children;
+    }
+    return acc;
+  }, []);
 };
-console.log("Result: ", getComponent(myimport.node));
+console.log(flatten(myimport.node.content));
+// console.log("Result: ", getComponent(myimport.node));
+
 export default {};
