7fbfd7840bb8011407c42d02fe4263f91c2b7ee7
vladgrecu <vladgrecu.smp@gmail.com>
2020-02-05 17:50:23 +0200

    started to work on attribute fix

--- components/UIDLtoHTMLComponent.tsx
+++ components/UIDLtoHTMLComponent.tsx
@@ -1,16 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import UIDLToHtml from "../utils/UIDLToHtml";
 import UIDLParser from "../utils/UIDLParser";
-import Interweave from "interweave"
-import { polyfillDOMImplementation } from 'interweave-ssr';
-
-//for server-side rendering part, required by interweave library (lightweight)
-polyfillDOMImplementation();
-
 export default function UIDLtoHTMLComponent(props): any {
-  
-  const [html, setHtml] = useState("");
-
   useEffect(() => {
     let UIDLObject: unknown;
     try {
@@ -18,10 +9,11 @@ export default function UIDLtoHTMLComponent(props): any {
     } catch (e) {
       UIDLObject = "";
     }
-    
-    const {html, style} = UIDLToHtml(UIDLParser(JSON.parse(JSON.stringify(UIDLObject))));
-    
-    setHtml(html)
+    console.log(UIDLParser(JSON.parse(JSON.stringify(UIDLObject))));
+    const { html, style } = UIDLToHtml(
+      UIDLParser(JSON.parse(JSON.stringify(UIDLObject)))
+    );
+    document.getElementById("htmlContainer").innerHTML = html;
 
     if (document.getElementById("generatedElementStyle")) {
       document.getElementById("generatedElementStyle").innerHTML = style;
@@ -32,15 +24,12 @@ export default function UIDLtoHTMLComponent(props): any {
     sheet.innerHTML = style;
     sheet.id = "generatedElementStyle";
     document.body.appendChild(sheet);
-
-    //maybe refactor here to use refs to watch props instea (?)
-  }, [props])
+  }, [props]);
 
   return (
     <div className="container">
-
-      <div className="htmlWrapper" >
-        <div id="htmlContainer" className="htmlContainer"><Interweave content={html}/></div>
+      <div className="htmlWrapper">
+        <div id="htmlContainer" className="htmlContainer"></div>
       </div>
 
       <style jsx>{`
@@ -49,15 +38,46 @@ export default function UIDLtoHTMLComponent(props): any {
           height: 100%;
           width: 100%;
           margin: auto;
+          background-color: white;
         }
         .htmlWrapper {
           position: relative;
           height: 100%;
           width: 100%;
+          border-radius: 6px;
+          background: #0b032d;
         }
         .container {
           height: 100%;
           width: 100%;
+        }
+        .buton {
+          position: relative;
+          display: block;
+          margin: 5% auto 0;
+          padding: 14px 15px;
+          color: #fff;
+          font-size: 14px;
+          font-weight: bold;
+          text-align: center;
+          text-decoration: none;
+          text-transform: uppercase;
+          overflow: hidden;
+          letter-spacing: 0.08em;
+          border-radius: 0;
+          text-shadow: 0 0 1px rgba(0, 0, 0, 0.2), 0 1px 0 rgba(0, 0, 0, 0.2);
+          transition: all 1s ease;
+          border: 2px solid #d24d57;
+        }
+
+        .buton:hover {
+          background: #d24d57;
+          transform: translateX(-50%) translateY(-50%) rotate(-25deg);
+        }
+
+        .buton {
+          color: #0b032d;
+          cursor: pointer;
         }
       `}</style>
     </div>
